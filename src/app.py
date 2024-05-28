e7f72c4a54d5f2bdd90a3f6842434407884bb9a9
santiagommarino <153780787+santiagommarino@users.noreply.github.com>
2024-05-28 20:56:41 +0000

    finished

--- src/app.py
+++ src/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify, request 
 app = Flask(__name__)
-# Add the jsonify method to your Flask import
 
 # Suppose you have your data in the variable named some_data
 todos = [
@@ -10,20 +9,32 @@ todos = [
 
 @app.route('/todos', methods=['GET'])
 def hello_world():
-# You can convert that variable into a json string like this
-    json_text = jsonify(some_data)
-    # And then you can return it to the front end in the response body like this
-    return json_text
-
-@app.route('/todos', methods=['POST'])
-def add_new_todo():
-    body = request.json
-    print("Incoming request with the following body", body)
-    todos.append(body)
     return jsonify(todos)
 
 
 
-# These two lines should always be at the end of your app.py file
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3245, debug=True)
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    print("Incoming request with the following body", request_body)
+    if "label" in request_body and "done" in request_body:
+        if isinstance(request_body["label"], str) and isinstance(request_body["done"],bool):
+            todos.append(request_body)
+            return jsonify(todos)
+
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    if 0 <= position < len(todos):
+        todos.pop(position)
+    return jsonify(todos)
+
+
+
+
+
+
+
+
+
+
